7b8bb6cd2714a452fedc23dcb951062062eb6756
Mário Santos <mariodev7@gmail.com>
2023-08-28 13:20:27 -0300

    create env validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './database/prisma/prisma.service'
 import { AccountsModule } from './accounts/accounts.module'
+import { envSchema } from './env'
 
 @Module({
   controllers: [],
   providers: [PrismaService],
   exports: [PrismaService],
-  imports: [AccountsModule],
+  imports: [
+    AccountsModule,
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
